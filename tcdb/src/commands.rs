bce87534758da13ef7d0034b4df9ecf97dc41a9e
Rafael Gonçalves <38723613+llrafaell@users.noreply.github.com>
2022-07-28 23:12:41 -0300

    Add test to commands (#13)
    * Add test to commands
    
    * Fix format
    
    Co-authored-by: grafael <grafael@national-platform.ru>

--- tcdb/src/commands.rs
+++ tcdb/src/commands.rs
@@ -180,3 +180,78 @@ impl CommandParser {
         Command::try_from_binary(instruction, &tokens[1])
     }
 }
+
+#[test]
+fn validate_return_all_instructions() {
+    let flag = 0b1111111111111;
+    let result = CommandParser::default()
+        .instructions()
+        .iter()
+        .fold(0, |bit, instruction| match instruction {
+            Instruction::Afore => bit | 0b1000000000000,
+            Instruction::Breakpoint => bit | 0b0100000000000,
+            Instruction::Continue => bit | 0b0010000000000,
+            Instruction::Delete => bit | 0b0001000000000,
+            Instruction::Goto => bit | 0b0000100000000,
+            Instruction::Help => bit | 0b0000010000000,
+            Instruction::Next => bit | 0b0000001000000,
+            Instruction::Open => bit | 0b0000000100000,
+            Instruction::Print => bit | 0b0000000010000,
+            Instruction::Restart => bit | 0b0000000001000,
+            Instruction::Turn => bit | 0b0000000000100,
+            Instruction::Quit => bit | 0b0000000000010,
+            Instruction::Witness => bit | 0b0000000000001,
+        });
+    assert_eq!(flag, result);
+
+    let a = CommandParser::default().instructions().to_vec();
+    let mut b = CommandParser::default().instructions().to_vec();
+
+    b.as_mut_slice().sort();
+    b.dedup();
+
+    assert_eq!(a.len(), b.len());
+}
+#[test]
+fn validade_parse_completable() {
+    let cases_instructions = vec![
+        ("ope", "n", Instruction::Open),
+        ("open", " ", Instruction::Open),
+        ("open Carg", "o.toml", Instruction::Open),
+    ];
+
+    let cases_empty = vec![" "];
+
+    let cases_invalid = vec!["Open "];
+
+    let cases_instructions =
+        cases_instructions
+            .into_iter()
+            .map(|(input, completion, instruction)| {
+                (
+                    input,
+                    ParsedLine::Completable {
+                        instruction,
+                        completion: completion.to_string(),
+                    },
+                )
+            });
+
+    let cases_empty = cases_empty
+        .into_iter()
+        .map(|input| (input, ParsedLine::Empty));
+
+    let cases_invalid = cases_invalid
+        .into_iter()
+        .map(|input| (input, ParsedLine::Invalid));
+
+    let parser = CommandParser::default();
+    cases_instructions
+        .chain(cases_empty)
+        .chain(cases_invalid)
+        .for_each(|(input, parsedline)| {
+            let partial_parse = parser.parse_completable(input).unwrap();
+
+            assert_eq!(partial_parse, parsedline);
+        });
+}
