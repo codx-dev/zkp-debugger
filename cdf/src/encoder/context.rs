0991aa227cbf2f4e0712e01134073261c05aaeea
Victor Lopez <victor@codx.io>
2022-08-30 12:21:30 +0200

    Add coverage for encoder context

--- cdf/src/encoder/context.rs
+++ cdf/src/encoder/context.rs
@@ -52,3 +52,52 @@ impl Deref for EncoderContext {
         &self.path_cache
     }
 }
+
+#[test]
+fn path_cache_is_not_duplicated() {
+    let main = PathBuf::from("home").join("zkp-debugger").join("main.rs");
+    let lib = PathBuf::from("home").join("zkp-debugger").join("lib.rs");
+
+    let mut context = EncoderContext::from_preamble(Default::default());
+
+    let idx_main = context.add_path(main.clone());
+
+    // duplicated path is not added; same index should be returned
+    assert_eq!(idx_main, context.add_path(main.clone()));
+
+    let idx_lib = context.add_path(lib.clone());
+
+    // lib is a different path so it should have a different index
+    assert_ne!(idx_main, idx_lib);
+}
+
+#[test]
+fn context_derives_expected_map() {
+    let main = PathBuf::from("home").join("zkp-debugger").join("main.rs");
+    let lib = PathBuf::from("home").join("zkp-debugger").join("lib.rs");
+
+    let mut context = EncoderContext::from_preamble(Default::default());
+
+    let idx_main = context.add_path(main.clone());
+    let idx_lib = context.add_path(lib.clone());
+
+    let expected_map: HashMap<PathBuf, usize> = vec![(main.clone(), idx_main), (lib, idx_lib)]
+        .into_iter()
+        .collect();
+
+    // deref op should extend map methods to context
+    assert_eq!(expected_map[&main], context[&main]);
+
+    // resulting map should equal expect
+    assert_eq!(*context, expected_map);
+}
+
+#[test]
+fn preamble_is_correctly_created() {
+    // TODO test all permutations of preamble in integration/fuzz
+    let preamble = Preamble::default();
+    let context = EncoderContext::from_preamble(preamble);
+
+    // context was created with the right preamble
+    assert_eq!(&preamble, context.preamble());
+}
