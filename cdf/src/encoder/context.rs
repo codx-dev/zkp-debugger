efa5994c47699094ba2cd100eca91ab316ed7ebe
Victor Lopes <victor@codx.io>
2022-09-13 16:15:31 +0200

    Add `EncodingContextProvider` (#38)
    It is desirable to generalize the context provider of the encoding to a
    map of string to string instead of requiring a disk read.
    
    The filesystem is one of many providers of an encoding context, and it
    is now available via a `EncodingContextFileProvider` struct.
    
    This will facilitate the testing functions as well as make the encoding
    process more flexible and customizable by the user.

--- cdf/src/encoder/context.rs
+++ cdf/src/encoder/context.rs
@@ -1,8 +1,32 @@
 use std::collections::HashMap;
 use std::ops::Deref;
 use std::path::PathBuf;
+use std::{fs, io};
+
+use msgpacker::Message;
 
 use crate::{Config, Preamble};
+
+/// Encoding provider that will convert paths into file contents
+pub trait EncoderContextProvider: Default {
+    /// Fetch the contents of a given path
+    fn contents<P>(&mut self, path: P) -> io::Result<String>
+    where
+        P: AsRef<str>;
+}
+
+/// Default encoding provider with a filesystem backend
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct EncoderContextFileProvider;
+
+impl EncoderContextProvider for EncoderContextFileProvider {
+    fn contents<P>(&mut self, path: P) -> io::Result<String>
+    where
+        P: AsRef<str>,
+    {
+        fs::read_to_string(path.as_ref())
+    }
+}
 
 /// Context of encoding a CDF file
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
@@ -42,6 +66,41 @@ impl EncoderContext {
         let len = self.path_cache.len();
 
         *self.path_cache.entry(path).or_insert(len)
+    }
+}
+
+impl EncoderContext {
+    pub fn write_all<P, W>(&self, mut writer: W) -> io::Result<usize>
+    where
+        P: EncoderContextProvider,
+        W: io::Write,
+    {
+        let mut provider = P::default();
+        let mut contents = self
+            .path_cache
+            .iter()
+            .map(|(p, i)| p.canonicalize().map(|p| (p, i)))
+            .collect::<io::Result<Vec<_>>>()?;
+
+        contents.as_mut_slice().sort_by_key(|(_p, i)| *i);
+
+        let paths = contents
+            .iter()
+            .map(|(p, _i)| format!("{}", p.display()))
+            .collect::<Vec<_>>();
+
+        let contents = paths
+            .iter()
+            .map(|p| provider.contents(p))
+            .map(|p| p.map(Message::String))
+            .collect::<io::Result<Vec<_>>>()?;
+
+        let paths = paths.into_iter().map(Message::String).collect();
+
+        let n = Message::Array(paths).pack(&mut writer)?;
+        let n = n + Message::Array(contents).pack(&mut writer)?;
+
+        Ok(n)
     }
 }
 
